9409526c9879940185e90ba6359c9887d8e133a4
davednguyen <davednguyen@gmail.com>
2019-03-09 17:18:36 -0800

    updates

---
+++ ImageProcessorAPI/ImageFunctions/iRotateImage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageProcessorAPI.ImageFunctions
+{
+    public interface iRotateImage
+    {
+        string RotateImageLeft(string imageFile);
+        string RotateImageRight(string imageFile);
+    }
+}
---
+++ ImageProcessorAPI/Models/ImageDataV2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageProcessorAPI.Models
+{
+    public class ImageDataV2
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string SourceImageFile { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Dictionary<string,bool> Requests { get; set; }
+        public string Image { get; set; }
+    }
+}
